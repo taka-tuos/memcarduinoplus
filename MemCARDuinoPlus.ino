519be13b14b49c8d032304aa1bbe17a5f29f27af
taka-tuos <hvcnes@gmail.com>
2017-10-15 11:08:01 +0900

    Add an Code that for Arduino and PC

---
+++ MemCARDuinoPlus.ino
@@ -0,0 +1,220 @@
+/*
+  MemCARDuino - Arduino PlayStation 1 Memory Card reader
+  Shendo 2013. - 2016.
+
+  Compatible with Arduino/Genuino Uno, Duemilanove, Diecimila, Nano, Mini,
+  basically any board with ATmega168/P or ATmega328/P.
+*/
+
+#include "Arduino.h"
+#include <SPI.h>
+
+//Device Firmware identifier
+#define IDENTIFIER "MCDPLS"  //MemCARDuinoPlus
+#define VERSION 0x10         //Firmware version byte (Major.Minor)
+
+//Commands
+#define GETID 0xA0          //Get identifier
+#define GETVER 0xA1         //Get firmware version
+#define MCREAD 0xA2         //Memory Card Read (frame)
+#define MCWRITE 0xA3        //Memory Card Write (frame)
+
+//Responses
+#define ERROR 0xE0         //Invalid command received (error)
+
+//Memory Card Responses
+//0x47 - Good
+//0x4E - BadChecksum
+//0xFF - BadSector
+
+//Define pins
+#define DataPin 12         //Data
+#define CmdPin 11          //Command
+#define AttPin 10          //Attention (Select)
+#define ClockPin 13        //Clock
+#define AckPin 2           //Acknowledge
+
+byte ReadByte = 0;
+volatile int state = HIGH;
+bool CompatibleMode = false;
+
+//Set up pins for communication
+void PinSetup()
+{
+  byte clr = 0;
+
+  pinMode(DataPin, INPUT);
+  pinMode(CmdPin, OUTPUT);
+  pinMode(AttPin, OUTPUT);
+  pinMode(ClockPin, OUTPUT);
+  pinMode(AckPin, INPUT);
+
+  //Set up SPI on Arduino (250 kHz, clock active when low, reading on falling edge of the clock)
+  SPCR = 0x7F;
+  clr=SPSR;
+  clr=SPDR;
+
+  digitalWrite(DataPin, HIGH);    //Activate pullup resistor
+  digitalWrite(CmdPin, LOW);
+  digitalWrite(AttPin, HIGH);
+  digitalWrite(ClockPin, HIGH);
+  digitalWrite(AckPin, HIGH);    //Activate pullup resistor
+
+  //Set up interrupt on pin 2 (INT.0) for Acknowledge signal
+  attachInterrupt(0, ACK, FALLING);
+}
+
+//Acknowledge routine
+void ACK()
+{
+  state = LOW;
+}
+
+//Send a command to PlayStation port using SPI
+byte SendCommand(byte CommandByte, int Delay)
+{
+    if(!CompatibleMode) Delay = 3000;   //Timeout
+    state = HIGH;                       //Set high state for ACK signal
+
+    SPDR = CommandByte;                 //Start the transmission
+    while (!(SPSR & (1<<SPIF)));        //Wait for the end of the transmission
+
+    //Wait for the ACK signal from the Memory Card
+    while(state == HIGH)
+    {
+      Delay--;
+      delayMicroseconds(1);
+      if(Delay == 0){
+        //Timeout reached, card doesn't report ACK properly
+        CompatibleMode = true;
+        break;
+      }
+    }
+
+  return SPDR;                    //Return the received byte
+}
+
+//Read a frame from Memory Card and send it to serial port
+void ReadFrame(unsigned int Address)
+{
+  byte AddressMSB = Address & 0xFF;
+  byte AddressLSB = (Address >> 8) & 0xFF;
+
+  //Use ACK detection mode by default
+  CompatibleMode = false;
+
+  //Activate device
+  PORTB &= 0xFB;    //Set pin 10 (AttPin, LOW)
+
+  SendCommand(0x81, 500);      //Access Memory Card
+  SendCommand(0x52, 500);      //Send read command
+  SendCommand(0x00, 500);      //Memory Card ID1
+  SendCommand(0x00, 500);      //Memory Card ID2
+  SendCommand(AddressMSB, 500);      //Address MSB
+  SendCommand(AddressLSB, 500);      //Address LSB
+  SendCommand(0x00, 2800);      //Memory Card ACK1
+  SendCommand(0x00, 2800);      //Memory Card ACK2
+  SendCommand(0x00, 2800);      //Confirm MSB
+  SendCommand(0x00, 2800);      //Confirm LSB
+
+  //Get 128 byte data from the frame
+  for (int i = 0; i < 128; i++)
+  {
+    Serial.write(SendCommand(0x00, 150));
+  }
+
+  Serial.write(SendCommand(0x00, 500));      //Checksum (MSB xor LSB xor Data)
+  Serial.write(SendCommand(0x00, 500));      //Memory Card status byte
+
+  //Deactivate device
+  PORTB |= 4;    //Set pin 10 (AttPin, HIGH)
+}
+
+//Write a frame from the serial port to the Memory Card
+void WriteFrame(unsigned int Address)
+{
+  byte AddressMSB = Address & 0xFF;
+  byte AddressLSB = (Address >> 8) & 0xFF;
+  byte ReadData[128];
+  int DelayCounter = 30;
+
+  //Use ACK detection mode by default
+  CompatibleMode = false;
+
+  //Activate device
+  PORTB &= 0xFB;    //Set pin 10 (AttPin, LOW)
+
+  SendCommand(0x81, 300);      //Access Memory Card
+  SendCommand(0x57, 300);      //Send write command
+  SendCommand(0x00, 300);      //Memory Card ID1
+  SendCommand(0x00, 300);      //Memory Card ID2
+  SendCommand(AddressMSB, 300);      //Address MSB
+  SendCommand(AddressLSB, 300);      //Address LSB
+
+  //Copy 128 bytes from the serial input
+  for (int i = 0; i < 128; i++)
+  {
+    while(!Serial.available());
+
+    ReadData[i] = Serial.read();
+  }
+
+  //Write 128 byte data to the frame
+  for (int i = 0; i < 128; i++)
+  {
+    SendCommand(ReadData[i], 150);
+  }
+
+  while(!Serial.available());
+
+  SendCommand(Serial.read(), 200);      //Checksum (MSB xor LSB xor Data)
+  SendCommand(0x00, 2800);               //Memory Card ACK1
+  SendCommand(0x00, 2800);               //Memory Card ACK2
+  Serial.write(SendCommand(0x00, 500)); //Memory Card status byte
+
+  //Deactivate device
+  PORTB |= 4;    //Set pin 10 (AttPin, HIGH)
+}
+
+void setup()
+{
+  //Set up serial communication
+  Serial.begin(38400);
+
+  //Set up pins
+  PinSetup();
+}
+
+void loop()
+{
+  //Listen for commands
+  if(Serial.available() > 0)
+  {
+    ReadByte = Serial.read();
+
+    switch(ReadByte)
+    {
+      default:
+        Serial.write(ERROR);
+        break;
+
+      case GETID:
+        Serial.write(IDENTIFIER);
+        break;
+
+      case GETVER:
+        Serial.write(VERSION);
+        break;
+
+      case MCREAD:
+      while(Serial.available() < 2);
+      ReadFrame(Serial.read() | Serial.read() << 8);
+        break;
+
+      case MCWRITE:
+      while(Serial.available() < 2);
+      WriteFrame(Serial.read() | Serial.read() << 8);
+        break;
+    }
+  }
+}
---
+++ main.c
@@ -0,0 +1,212 @@
+#include "serial.h"
+#include <time.h>
+#include <string.h>
+
+#if defined(__MINGW32__) || defined(_WIN32) || defined(__CYGWIN__)
+#include <windows.h>
+#define sleep(n) Sleep(n * 1000)
+#endif
+
+int mc_read(SERIAL *device, FILE *fp, int i)
+{
+	unsigned char block[130];
+	
+	unsigned char frame_lsb = i & 0xff;
+	unsigned char frame_msb = i >> 8;
+	
+	serial_write(device,"\xa2",1);
+	
+	serial_write(device,&frame_msb,1);
+	serial_write(device,&frame_lsb,1);
+	
+	unsigned char xor_data = (frame_msb ^ frame_lsb);
+	
+	while(serial_avaiable(device) < 130);
+	
+	serial_read(device,block,130);
+	
+	for(int j = 0; j < 128; j++)
+	{
+		xor_data ^= block[j];
+	}
+	
+	if(block[129] == 0x47 && block[128] == xor_data) {
+		fwrite(block,1,128,fp);
+		i++;
+		printf("BLOCK %d READ\n",i);
+	}
+	
+	return i;
+}
+
+int mc_write(SERIAL *device,FILE *fp, int i)
+{
+	unsigned char block[130];
+	
+	unsigned char frame_lsb = i & 0xff;
+	unsigned char frame_msb = i >> 8;
+	
+	serial_write(device,"\xa3",1);
+	
+	serial_write(device,&frame_msb,1);
+	serial_write(device,&frame_lsb,1);
+	
+	unsigned char xor_data = (frame_msb ^ frame_lsb);
+	
+	fread(block,1,128,fp);
+	
+	for(int j = 0; j < 128; j++)
+	{
+		xor_data ^= block[j];
+	}
+	
+	serial_write(device,block,128);
+	serial_write(device,&xor_data,1);
+	
+	while(serial_avaiable(device) < 1);
+	
+	serial_read(device,block,1);
+	
+	if(block[0] == 0x47) {
+		i++;
+		printf("BLOCK %d WRITE\n",i);
+	} else {
+		fseek(fp,-128,SEEK_CUR);
+	}
+	
+	return i;
+}
+
+int main(int argc, char *argv[])
+{
+	char portname[256] = "";
+	char filename[256] = "";
+	int rw_mode = -1;
+	char *to_write = NULL;
+	
+	if(argc == 0) {
+		printf("usage : %s -p Serialport [-r Filename] [-w Filename] [-c] [-help]\n\n",argv[0]);
+		printf("-p    : Specify serial port(Required)\n");
+		printf("-r    : Specify the data load destination file(Optional)\n");
+		printf("-w    : Specify the data writing source file(Optional)\n");
+		printf("-c    : Specify auto-naming mode (read-only, -r and -w are ignored)(Optional)\n");
+		printf("-help : Show this message\n");
+		return 1;
+	}
+	
+	for(int i = 1; i < argc; i++) {
+		if(strcmp(argv[i],"-help") == 0) {
+			printf("usage : %s -p Serialport [-r Filename] [-w Filename] [-c] [-help]\n\n",argv[0]);
+			printf("-p    : Specify serial port(Required)\n");
+			printf("-r    : Specify the data load destination file(Optional)\n");
+			printf("-w    : Specify the data writing source file(Optional)\n");
+			printf("-c    : Specify auto-naming mode (read-only, -r and -w are ignored)(Optional)\n");
+			printf("-help : Show this message\n");
+			return 1;
+		}
+		
+		if(to_write != NULL) {
+			strcpy(to_write,argv[i]);
+			to_write = NULL;
+			continue;
+		} else {
+			if(strcmp(argv[i],"-p") == 0) {
+				to_write = portname;
+			}
+			else if(strcmp(argv[i],"-r") == 0) {
+				if(rw_mode != -1) {
+					printf("The options \"-r\" and \"-w\" can not be used at the same time.\n");
+					return 2;
+				}
+				to_write = filename;
+				rw_mode = 0;
+			}
+			else if(strcmp(argv[i],"-w") == 0) {
+				if(rw_mode != -1) {
+					printf("The options \"-r\" and \"-w\" can not be used at the same time.\n");
+					return 2;
+				}
+				to_write = filename;
+				rw_mode = 1;
+			}
+			else if(strcmp(argv[i],"-c") == 0) {
+				time_t utctime = time(NULL);
+				struct tm *local = localtime(&utctime);
+				sprintf(filename,"%d-%d-%d-%02d-%02d-%02d.mcr",
+					local->tm_year+1900,
+					local->tm_mon+1,
+					local->tm_mday,
+					local->tm_hour,
+					local->tm_min,
+					local->tm_sec);
+				rw_mode = 0;
+			}
+		}
+	}
+	
+	if(filename[0] == 0) {
+		puts("nothing to do.");
+		return 3;
+	}
+	
+	if(portname[0] == 0) {
+		puts("fatal : Serial port designation is a required option.");
+		return 4;
+	}
+	
+	SERIAL *device = serial_open(portname, SerialBaud38400);
+	
+	if(!device) {
+		puts("fatal : Failed to open serial port.");
+		return 5;
+	}
+	
+	char id[7];
+	
+	sleep(2);
+	
+	puts("Detecting device ...");
+	serial_write(device,"\xa0",1);
+	if(serial_read_with_timeout(device,id,6,10000)) {
+		puts("fatal : Connection failed.");
+	} else {
+		if(strncmp(id,"MCDINO",6) == 0) {
+			puts("Detected MemCARDuino.");
+		}
+		else if(strncmp(id,"MCDPLS",6) == 0) {
+			puts("Detected MemCarDuinoPlus.");
+		}
+		else {
+			puts("fatal : Unknown Device");
+			//puts(id);
+			return 7;
+		}
+		unsigned char version;
+		serial_write(device,"\xa1",1);
+		while(serial_avaiable(device) < 1);
+		serial_read(device,&version,1);
+		int version_major = version >> 4, version_minor = version & 0xf;
+		printf("Version : %d.%d\n",version_major,version_minor);
+	}
+	
+	
+	FILE *fp = fopen(filename, rw_mode == 0 ? "wb" : "rb");
+	
+	if(!fp) {
+		printf("fatal : Failed to open the filename %s.\n",filename);
+		return 6;
+	}
+	
+	int i = 0;
+	
+	while(i < 1024)
+	{
+		if(rw_mode == 0) i = mc_read(device,fp,i);
+		if(rw_mode == 1) i = mc_write(device,fp,i);
+	}
+	
+	fclose(fp);
+	serial_close(device);
+	
+	return 0;
+}
